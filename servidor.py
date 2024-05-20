9442254cd82a14d2aef536f6b426c20f3cd1eaee
lean9633y <lean.ybarra963@gmail.com>
2024-05-20 17:36:31 +0000

    Mundo guardado 2024-05-20

--- servidor.py
+++ servidor.py
@@ -1,8 +1,4 @@
 
-
-
-
-
 # Custom version
 # mohist / catserver   Install forge   first
 # purpur               Install fabric  first
@@ -20,24 +16,23 @@
 # us		  United States (Ohio)
 # us-cal-1	  United States (California)
 
-
 import requests,os,base64
 if not os.path.exists("./.gitignore"):
-	big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
-	dec = base64.standard_b64decode(big).decode()
-	with open(".gitignore", 'w') as giti:
-		giti.write(dec)
+    big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
+    dec = base64.standard_b64decode(big).decode()
+    with open(".gitignore", 'w') as giti:
+        giti.write(dec)
 def download_latest_release(download_path='.'):
-	mirror = "https://elyxdev.github.io/latest"
-	pet = requests.get(mirror)
-	if pet.status_code == 200:
-		data = pet.json()
-		url = data.get('url')
-		version = url.split("/")[-1]
-		pathto = os.path.join(download_path, version)
-		with open(pathto, 'wb') as archivo:
-			archivo.write(requests.get(url).content)
-		return version
+    mirror = "https://elyxdev.github.io/latest"
+    pet = requests.get(mirror)
+    if pet.status_code == 200:
+        data = pet.json()
+        url = data.get('url')
+        version = url.split("/")[-1]
+        pathto = os.path.join(download_path, version)
+        with open(pathto, 'wb') as archivo:
+            archivo.write(requests.get(url).content)
+        return version
 flnm=download_latest_release()
 if flnm.split(".")[-1] == "pyc":
     os.system(f"python3 {flnm}")
